11256a8bbd3cdb8a543d9c31cf30e3fbda6c6a09
aryan-deshmukh45 <aryandeshmukh947@gmail.com>
2026-02-22 00:26:05 +0530

    Placing orders

--- schemas.py
+++ schemas.py
@@ -40,3 +40,19 @@ class LoginModel(BaseModel):
     password:str
     
 
+class OrderModel(BaseModel):
+    id:Optional[int]  
+    quantity:int
+    order_status:Optional[str]="PENDING"
+    pizza_size:Optional[str]="SMALL"
+    user_id:Optional[int]
+    user:Optional[int]
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            "example":{
+                "quantity":2,
+                "pizza_size":"LARGE"
+            }
+        }
